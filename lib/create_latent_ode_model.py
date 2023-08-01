578ff7093cb1d3d2f5e6cb44a447f6d2536b08b6
zwj1203 <3200100063@zju.edu.cn>
2023-07-31 21:49:55 -0700

    reverse_func

--- lib/create_latent_ode_model.py
+++ lib/create_latent_ode_model.py
@@ -1,7 +1,7 @@
 from lib.gnn_models import GNN
 from lib.latent_ode import LatentGraphODE
 from lib.encoder_decoder import *
-from lib.diffeq_solver import DiffeqSolver,GraphODEFunc
+from lib.diffeq_solver import DiffeqSolver,GraphODEFunc,ReverseGraphODEFunc
 
 
 
@@ -30,11 +30,14 @@ def create_LatentODE_model(args, input_dim, z0_prior, obsrv_std, device):
 
 	ode_func_net = GNN(in_dim = ode_input_dim,n_hid =ode_dim,out_dim = ode_input_dim,n_heads=args.n_heads,n_layers=args.gen_layers,dropout=args.dropout,conv_name = args.odenet,aggregate="add")
 
-	gen_ode_func = GraphODEFunc(
+	gen_ode_func= GraphODEFunc(
+		ode_func_net=ode_func_net,
+		device=device).to(device)
+	reverse_gen_ode_func=ReverseGraphODEFunc(
 		ode_func_net=ode_func_net,
 		device=device).to(device)
 
-	diffeq_solver = DiffeqSolver(gen_ode_func, args.solver, args=args,odeint_rtol=1e-2, odeint_atol=1e-2, device=device)
+	diffeq_solver = DiffeqSolver(gen_ode_func,reverse_gen_ode_func, args.solver, args=args,odeint_rtol=1e-2, odeint_atol=1e-2, device=device)
 
     #Decoder related
 	decoder = Decoder(latent_dim, input_dim).to(device)
